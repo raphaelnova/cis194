b2b31a06f65a54989530c9fd311d92e55dd493e3
Raphael Nova <rafa.boanova@gmail.com>
2015-01-06 01:03:39 -0200

    Ex. 2 -- doubleEveryOther

--- src/Week1/CreditCard.hs
+++ src/Week1/CreditCard.hs
@@ -1,10 +1,25 @@
 module Week1.CreditCard where
 
-toDigits :: Int -> [Int]
+-- Ex. 1
+toDigits :: Integer -> [Integer]
 toDigits = reverse . toDigitsRev
 
-toDigitsRev :: Int -> [Int]
+toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
     | n <= 0    = []
     | otherwise = n `mod` 10 : toDigitsRev (n `div` 10)
+
+-- Ex. 2
+-- go down the stack and work your way up doing what you must
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = snd . countFromBottom
+  where
+    countFromBottom :: [Integer] -> (Int, [Integer])
+    countFromBottom []     = (0, [])
+    countFromBottom (x:xs) =
+        let (prevIdx, ys)  = countFromBottom xs
+            currIdx        = prevIdx + 1
+        in if even currIdx
+            then (currIdx, 2*x : ys)
+            else (currIdx,   x : ys)
 
--- test/Week1.hs
+++ test/Week1.hs
@@ -7,7 +7,7 @@ import Week1.CreditCard
 
 specs :: SpecWith ()
 specs = do
-    describe "Week 1 - Credit card" $ do
+    describe "Week 1 - Ex. 1" $ do
         describe "toDigits" $ do
             it "returns an empty list for negative numbers" $ do
                 toDigits (-42) `shouldBe` []
@@ -16,12 +16,21 @@ specs = do
             it "returns the correct digits" $ do
                 toDigits 12345 `shouldBe` [1,2,3,4,5]
                 toDigits 100   `shouldBe` [1,0,0]
-
         describe "toDigitsRev" $ do
             it "returns an empty list for negative numbers" $ do
                 toDigitsRev (-42) `shouldBe` []
             it "returns an empty list for zero" $ do
                 toDigitsRev 0 `shouldBe` []
             it "is the reverse list of toDigits" $ property $ \n ->
-                toDigitsRev n `shouldBe` reverse (toDigits n)
+                toDigitsRev n == reverse (toDigits n)
+    describe "Week 1 - Ex. 2" $ do
+        describe "doubleEveryOther" $ do
+            it "outputs a list the same size of the input" $ property $ \ls ->
+                length (doubleEveryOther ls) == length ls
+            it "doesn't change a singleton list" $ property $ \n ->
+                doubleEveryOther [n] == [n]
+            it "counts from the end of the list" $ property $ \e o ->
+                doubleEveryOther       [e, o] ==         [2*e, o] &&
+                doubleEveryOther    [o, e, o] ==      [o, 2*e, o] &&
+                doubleEveryOther [e, o, e, o] == [2*e, o, 2*e, o]
 
