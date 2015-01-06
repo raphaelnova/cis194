d068f87c47d90f8a47d529c451f99f891c70e1d2
Raphael Nova <rafa.boanova@gmail.com>
2015-01-05 23:52:24 -0200

    Ex. 1 -- toDigits and toDigitsRev

--- src/Week1/CreditCard.hs
+++ src/Week1/CreditCard.hs
@@ -1,5 +1,10 @@
 module Week1.CreditCard where
 
 toDigits :: Int -> [Int]
-toDigits n = [n]
+toDigits = reverse . toDigitsRev
+
+toDigitsRev :: Int -> [Int]
+toDigitsRev n
+    | n <= 0    = []
+    | otherwise = n `mod` 10 : toDigitsRev (n `div` 10)
 
--- test/Week1.hs
+++ test/Week1.hs
@@ -9,6 +9,19 @@ specs :: SpecWith ()
 specs = do
     describe "Week 1 - Credit card" $ do
         describe "toDigits" $ do
-            it "always returns a singleton list" $ property $ \n ->
-                toDigits n == [n]
+            it "returns an empty list for negative numbers" $ do
+                toDigits (-42) `shouldBe` []
+            it "returns an empty list for zero" $ do
+                toDigits 0 `shouldBe` []
+            it "returns the correct digits" $ do
+                toDigits 12345 `shouldBe` [1,2,3,4,5]
+                toDigits 100   `shouldBe` [1,0,0]
+
+        describe "toDigitsRev" $ do
+            it "returns an empty list for negative numbers" $ do
+                toDigitsRev (-42) `shouldBe` []
+            it "returns an empty list for zero" $ do
+                toDigitsRev 0 `shouldBe` []
+            it "is the reverse list of toDigits" $ property $ \n ->
+                toDigitsRev n `shouldBe` reverse (toDigits n)
 
